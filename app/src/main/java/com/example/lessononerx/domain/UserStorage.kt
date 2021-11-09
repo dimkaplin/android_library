7c80ade6b3944235d5183ff1647c86c9ee43e95b
Kaplin Dima <Korvin777KDS@mail.ru>
2021-11-09 22:23:26 +0300

    RX part 3

--- app/src/main/java/com/example/lessononerx/domain/UserStorage.kt
+++ app/src/main/java/com/example/lessononerx/domain/UserStorage.kt
@@ -1,6 +1,9 @@
 package com.example.lessononerx.domain
 
+import io.reactivex.Observable
+
 interface UserStorage {
+    val users: Observable<List<UserProfile>>
     fun saveUser(user: UserProfile)
     fun getUsers(): List<UserProfile>
 }
--- app/src/main/java/com/example/lessononerx/ui/ClubActivity.kt
+++ app/src/main/java/com/example/lessononerx/ui/ClubActivity.kt
@@ -4,29 +4,74 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.widget.Toast
 import com.example.lessononerx.R
 import com.example.lessononerx.databinding.ActivityClubBinding
+import com.example.lessononerx.domain.City
 import com.example.lessononerx.domain.UserProfile
+import com.example.lessononerx.domain.UserStorage
+import com.example.lessononerx.impl.UserRoomStorageImpl
+import com.google.android.material.snackbar.Snackbar
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.disposables.Disposable
 
 class ClubActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityClubBinding
+    //private var currentDisposable: Disposable? = null
+    private var compositeDisposable: CompositeDisposable = CompositeDisposable()
+    /*set(value) {
+        field?.takeIf { !it.isDisposed }?.dispose()
+        field = value
+    }*/
     companion object {
        val USER_NAME = "name of user"
-        fun createLaunchIntent(context:Context, userLogin: String): Intent {
+        private val listUserProfile: UserStorage = UserRoomStorageImpl()
+        fun createLaunchIntent(context:Context, userLogin: String, userProfile:UserProfile): Intent {
             val intent = Intent(context, ClubActivity::class.java)
             if (userLogin != "") {
                 intent.putExtra(ClubActivity.USER_NAME, userLogin)
             }
+            listUserProfile.saveUser(userProfile)
             return intent;
         }
         fun createLaunchIntent(context:Context): Intent {
-            return createLaunchIntent(context, "")
+            return createLaunchIntent(context, "", UserProfile("test",0, 0, City("city"),"","", "", ""))
+        }
+        fun getUserStorage() : UserStorage {
+            return listUserProfile
         }
     }
-    private lateinit var binding: ActivityClubBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         binding = ActivityClubBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_club)
+        compositeDisposable.add(
+            getUserStorage().users
+                //.subscribeOn()
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe{
+                //Snackbar.make(binding.root, getString(R.string.after_add_user_message), Snackbar.LENGTH_SHORT).show()
+                Toast.makeText(applicationContext, getString(R.string.after_add_user_message), Toast.LENGTH_SHORT).show()
+                /*Handler(Looper.getMainLooper()).post {
+                    Toast.makeText(applicationContext, getString(R.string.after_add_user_message), Toast.LENGTH_SHORT).show()
+                }*/
+                //runOnUiThread {  }
+            }
+        )
+        /*currentDisposable = getUserStorage().users.subscribe{
+            Snackbar.make(binding.root, getString(R.string.after_add_user_message), Snackbar.LENGTH_SHORT).show()
+        }*/
+    }
+
+    override fun onDestroy() {
+        //currentDisposable?.dispose()
+        compositeDisposable.dispose()
+        super.onDestroy()
     }
 }
